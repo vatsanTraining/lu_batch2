3cfda13d5d7f018c5b1899d9fe32346081bffa84
vatsank <vatsank@gmail.com>
2020-12-07 12:04:12 +0530

    Update

--- DataHiding_Encapsulation/src/com/example/demo/Application.java
+++ DataHiding_Encapsulation/src/com/example/demo/Application.java
@@ -2,6 +2,15 @@ package com.example.demo;
 
 public class Application {
 
+	public static Double checkScholorship(Double markScored) {
+		
+		Double amount = 50000.00D;
+		if(markScored <70) {
+			amount = 35000.00D;
+		} 
+		return amount;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -36,7 +45,48 @@ public class Application {
 		
 		System.out.println(" Rajesh - Grade:= "+ rajesh.assignGrade());
 		
+		// Upcasting - cast not required
 		
+		byte rno = 103;
+		
+		Student manish = new Student();
+		
+		  manish.setRollNumber(rno);  // able to byte to int
+		  manish.setStudentName("Manish Kumar");
+		  manish.setMarkScored(67);
+		  
+		  
+		  System.out.println(manish.getMarkScored());
+			System.out.println(manish.getRollNumber());
+			System.out.println(manish.getStudentName());
+
+		  
+		  System.out.println("Manish - Grade:="+ manish.assignGrade());
+		  
+		  // Variable Casting - down casting
+		  
+		  int roll= 56;
+		  
+		  byte broll =(byte)roll;
+		  
+		  String mark ="66"; //correct -can parse
+		  String age ="SixtySix";// wrong - cannot parse Number Format Exception will be thrown
+		  
+		  int rollNumber = Integer.parseInt(args[0]);   // parsing String representation of number into integer
+		  
+		  double markScored = Double.parseDouble(args[2]);// parsing string to double
+		  
+		  
+		  Student rishi = new Student(rollNumber,args[1],markScored);
+		  
+		  
+		  System.out.println(rishi.getMarkScored());
+			System.out.println(rishi.getRollNumber());
+			System.out.println(rishi.getStudentName());
+		  
+		  System.out.println("Rishi - Grade:="+ rishi.assignGrade());
+
+		  checkScholorship(rishi.getMarkScored());
 		
 	}
 
